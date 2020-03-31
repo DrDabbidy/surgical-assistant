028a1005bd6838c2948b4dad5900c106d7f1d3f8
David De Martin <david.demartin123@gmail.com>
2020-03-31 12:02:08 -0400

    Added lang command
    Added command to get lang code for trans function

--- Surgical_Assistant_Commands.py
+++ Surgical_Assistant_Commands.py
@@ -5,6 +5,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from googletrans import Translator
+from googletrans import LANGUAGES
 
 # get the bot token from the file .env in same directory
 load_dotenv()
@@ -18,6 +19,13 @@ bot = commands.Bot(command_prefix="$")
 async def translate(ctx, dest_lang, *, message):
         translation = translator.translate(message, dest = dest_lang)
         await ctx.send(translation.origin + " :arrow_right: " + translation.text + "\n(" + translation.src + " :arrow_right: " + translation.dest + ")")
+
+@bot.command(name="lang", help="Shows the two letter language code for given language")
+async def show_lang(ctx, lang):
+        for code, language in googletrans.LANGUAGES.items:
+                if lang in language.lower():
+                        await ctx.send(code)
+                        return
 
 # run the bot on the discord server
 bot.run(TOKEN)
